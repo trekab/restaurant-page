c5b11ae208bbc01ec2879422a3b8c5122d0e5242
Treasure Kabareebe <treasurecalm@gmail.com>
2020-05-19 11:41:26 +0300

    Add about link functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 import aboutModule from './about';
 import menuModule from './menu';
+import aboutLinkModule from './aboutLink';
 
 const content = document.getElementById("content");
 content.className = "container mt-5 mb-5 pt-5 pb-5 rounded border border-light min-vh-100";
@@ -11,7 +12,13 @@ const navItems = document.querySelectorAll('.nav-link');
 
 const pageContent = document.querySelector('.card');
 
+navItems[0].addEventListener('click', () => {
+	pageContent.innerHTML = '';
+	aboutLinkModule(pageContent);
+});
+
 navItems[1].addEventListener('click', () => {
 	pageContent.innerHTML = '';
 	menuModule(pageContent);
 });
+
